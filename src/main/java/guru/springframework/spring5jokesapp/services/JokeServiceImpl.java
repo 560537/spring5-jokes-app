f504176b5fbd03e30d048732099dd565373f9eb0
560537 <magda.mathews@sap.com>
2022-02-08 11:01:07 +0100

    Created ChuckConfiguration. Closes #7

--- src/main/java/guru/springframework/spring5jokesapp/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesapp/services/JokeServiceImpl.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Service;
 public class JokeServiceImpl implements JokeService {
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
+    /*public JokeServiceImpl() {
         this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }*/
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
